bd04e5341ebea583ac61785b02df0387b6d31ea7
David Pratama <davidchristianpratama@gmail.com>
2023-11-21 12:06:37 +0700

    adding readme.md by using cody.ai

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
+// Import Apollo Server and schema 
 const { ApolloServer } = require("apollo-server");
 const { importSchema } = require("graphql-import");
-const EtherDataSource = require("./datasource/ethDatasource");
-const typeDefs = importSchema("./schema.graphql");
 
+// Import custom data source
+const EtherDataSource = require("./datasource/ethDatasource");  
+
+// Import GraphQL schema
+const typeDefs = importSchema("./schema.graphql"); 
+
+// Load environment variables
 require("dotenv").config();
 
+// Define GraphQL resolvers
 const resolvers = {
   Query: {
-    getEthByAddress: (root, _args, { dataSources }) =>
+    etherBalanceByAddress: (root, _args, { dataSources }) => 
+      // Call etherBalanceByAddress method on data source
       dataSources.ethDataSource.etherBalanceByAddress(),
-    getTotalSupplyEth: (root, _args, { dataSources }) =>
+
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
+      // Call totalSupplyOfEther method on data source  
       dataSources.ethDataSource.totalSupplyOfEther(),
-    //Paste Code for New Resolver Functions
-    getEthPrice: (root, _args, { dataSources }) =>
-    dataSources.ethDataSource.getLatestEthereumPrice(),
-    getEstimationTimePerTransaction: (root, _args, { dataSources }) =>
-    dataSources.ethDataSource.getBlockConfirmationTime(),
+
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      // Call getLatestEthereumPrice method on data source
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      // Call getBlockConfirmationTime method on data source
+      dataSources.ethDataSource.getBlockConfirmationTime(),
   },
 };
 
+// Create Apollo Server instance
 const server = new ApolloServer({
+  
+  // Pass in schema and resolvers
   typeDefs,
   resolvers,
+  
+  // Set up data sources
   dataSources: () => ({
-    ethDataSource: new EtherDataSource(),
+    ethDataSource: new EtherDataSource(), 
   }),
 });
 
+// Set timeout and start server
 server.timeout = 0;
 server.listen("9000").then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
